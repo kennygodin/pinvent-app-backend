fc2208a3df0b973981c4c07748ad8d3f016bf9af
kennycodin <kenechukwuokoh30@gmail.com>
2022-12-23 12:10:24 +0100

    https://pinvent-app-ten.vercel.app in server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'https://pinvent-app-psi.vercel.app'],
+    origin: ['http://localhost:3000', 'https://pinvent-app-ten.vercel.app'],
     credentials: true,
   })
 );
